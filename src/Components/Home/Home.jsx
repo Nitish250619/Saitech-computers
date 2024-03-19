d7db8cd2c2a6f8424b78f666f9818562075b2b45
Nitish250619 <68868020+Nitish250619@users.noreply.github.com>
2024-03-19 14:30:42 +0530

    completed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,15 +1,37 @@
 import React from "react";
-import "./Home.css"
+import "./Home.css";
+import Button from "@mui/material/Button";
 import CustomButton from "../CustomButton/CustomButton";
 
-
 const Home = () => {
+
+  const handleEnquiry = () => {
+    const whatsappNumber = "+919993514123";
+    const whatsappLink = `https://wa.me/${whatsappNumber}`;
+    window.open(whatsappLink, "_blank");
+  };
   return (
     <div className="container">
       <section className="hero">
         <p>Established And Reputable</p>
         <h1>Computer Peripheral Repairs</h1>
-        <CustomButton name={"Enquire Now"}/>
+        <div>
+          <Button
+          onClick={handleEnquiry}
+            variant="contained"
+            sx={{
+              color: "#f5b921", // Text color
+              backgroundColor: "#1f5156", // Background color
+              "&:hover": {
+                backgroundColor: "#153b40", // Darken background color on hover
+              },
+              borderRadius: "10px", // Adjust the border radius
+              padding: "12px 24px", // Adjust the padding to increase the size
+            }}
+          >
+            Enquiry nOW
+          </Button>
+        </div>
       </section>
     </div>
   );
